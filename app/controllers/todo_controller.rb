e899dc406f4ed64967f5aa9dadd36710ef753398
pk-almeida <patrickdepauloalmeida@gmail.com>
2025-08-20 21:11:23 -0300

    View Criar ToDo

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,4 +2,20 @@ class TodoController < ApplicationController
     def index
         @todos = Todo.all
     end
+
+    def new
+        @todo = Todo.new
+    end
+
+    def create
+        todo = Todo.create(todo_params)
+        todo.save
+        redirect_to todos_path
+    end
+
+    private
+
+    def todo_params
+        params.require(:todo).permit(:description, :completed)
+    end
 end
